cbc1d6f7cb4e2bc2ea122bedfdcc66efb07e6feb
YashRaut24 <yashcodezzzz24@gmail.com>
2025-10-16 18:19:03 +0530

    Created a basic input for to do list

--- frontend/my-app/src/components/HomePage.jsx
+++ frontend/my-app/src/components/HomePage.jsx
@@ -1,28 +1,22 @@
-import { useState, useRef } from "react";
-import CardsPage from "./CardsPage";
-import "./HomePage.css"
+import React, { useState } from "react";
+import "./HomePage.css";
+function HomePage(props){
 
-function HomePage(props) {
+  const[task,setTask] = useState("");
 
-  const list = [];
-
-  const addList = (event) => {
-    list++
-  }
-
-  return (
-    <div>
-      {list.map((list,index)=>{
-        <div key={index} className="taskListArea">
-            <div><input placeholder="Enter your task"  className={`${props.mode ? "darklist" : "list"} ${props.collapsed ? "expanded" : ""}`}/></div>
-            <button onClick={()=>{addList}} className="add-note">+</button>
-        </div>
-      })}
-    
-
-      {/* <CardsPage mode={props.mode} collapsed={props.collapsed} /> */}
+  return(
+    <div className={`task-container ${props.collapsed ? "expanded" : ""}`}>
+      <h1>Tasks</h1>
+      <input 
+        className={props.mode ? "darklist" : "list"}
+        type="text"
+        placeholder="Enter a task"
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
+      />
+      <p>Typed task: {task}</p>
     </div>
-  );
+  )
 }
 
 export default HomePage;
