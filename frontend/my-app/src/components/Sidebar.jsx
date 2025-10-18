ca5b66c2ab77051987e63e29ad5aaaf0a8a94276
YashRaut24 <yashcodezzzz24@gmail.com>
2025-10-18 21:47:26 +0530

    Created basic task containers

--- frontend/my-app/src/components/Sidebar.jsx
+++ frontend/my-app/src/components/Sidebar.jsx
@@ -1,6 +1,15 @@
+import { useState } from "react";
 import './Sidebar.css';
+import HomePage from "./HomePage";
 
 function Sidebar({ mode, collapsed, setCollapsed }) {
+
+  const[togglePage,setTogglePage] = useState();
+  
+  const setPage = () =>{
+    alert("You are in a task");
+  }
+
   return (
     <div className={`${mode ? "darkSidebar" : "sidebar"} ${collapsed ? "collapsed" : ""}`}>
       <button
@@ -9,13 +18,13 @@ function Sidebar({ mode, collapsed, setCollapsed }) {
       >
         {collapsed ? "→" : "←"}
       </button>
-      {!collapsed && <p className={mode ? "darkTask" : "task"}>
+      {!collapsed && <p onClick={setPage} className={mode ? "darkTask" : "task"}>
         Tasks 1
         </p>}
       {collapsed && <p className={mode? "darkST" : "ST"}>T1</p>}
 
-       {!collapsed && <p className={mode ? "darkTask" : "task"}>
-        Task 2
+       {!collapsed && <p onClick={setPage} className={mode ? "darkTask" : "task"}>
+        Tasks 2
         </p>}
       {collapsed && <p className={mode? "darkST" : "ST"}>T2</p>}
     </div>
