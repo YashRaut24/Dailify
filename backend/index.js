98427327453772b84cc0d19a73fea5d8ccc787a8
YashRaut24 <yashcodezzzz24@gmail.com>
2025-10-18 21:48:23 +0530

    Connected backend with db for the tasks

---
+++ backend/index.js
@@ -0,0 +1,31 @@
+let express = require("express");
+let app = express();
+let cors = require("cors");
+let mongodb = require("mongodb");
+let MongoClient = mongodb.MongoClient;
+
+app.use(cors());
+app.use(express.json());
+
+const url = "mongodb://localhost:27017";
+
+app.post("/add", (request, response) => {
+    let client = new MongoClient(url);
+    client.connect();
+    let db = client.db("Dailify");
+    let collec = db.collection("Tasks");
+    
+    let obj = {
+        task: request.body.task
+    };
+    
+    collec.insertOne(obj)
+        .then((result) => response.send(result))
+        .catch((error) => response.send(error));
+});
+
+
+
+app.listen(9000, () => {
+    console.log("Express is Active on port 9000");
+});
