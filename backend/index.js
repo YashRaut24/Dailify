0d94626c34460d4063334badc19d77fac5bf2805
YashRaut24 <yashcodezzzz24@gmail.com>
2025-10-18 22:11:14 +0530

    Added remove logic in backend

--- backend/index.js
+++ backend/index.js
@@ -24,7 +24,16 @@ app.post("/add", (request, response) => {
         .catch((error) => response.send(error));
 });
 
-
+app.delete("/delete", (request, response) => {
+    let client = new MongoClient(url);
+    client.connect();
+    let db = client.db("Dailify");
+    let collec = db.collection("Tasks");
+    
+    collec.deleteOne({ task: request.body.task })
+        .then((result) => response.send(result))
+        .catch((error) => response.send(error));
+});
 
 app.listen(9000, () => {
     console.log("Express is Active on port 9000");
