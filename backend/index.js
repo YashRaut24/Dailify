119025c01eb5b9b106acf0429a6de95a302cc6fb
YashRaut24 <yashcodezzzz24@gmail.com>
2025-10-20 21:24:52 +0530

    Updated backend for task containers in sidebar, and loaded them successfully

--- backend/index.js
+++ backend/index.js
@@ -16,12 +16,19 @@ app.post("/add", (request, response) => {
     let collec = db.collection("Tasks");
     
     let obj = {
-        task: request.body.task
+        task: request.body.task,
+        category: request.body.category
     };
     
     collec.insertOne(obj)
-        .then((result) => response.send(result))
-        .catch((error) => response.send(error));
+        .then((result) => {
+            client.close();
+            response.send(result);
+        })
+        .catch((error) => {
+            client.close();
+            response.send(error);
+        });
 });
 
 app.delete("/delete", (request, response) => {
@@ -30,9 +37,108 @@ app.delete("/delete", (request, response) => {
     let db = client.db("Dailify");
     let collec = db.collection("Tasks");
     
-    collec.deleteOne({ task: request.body.task })
-        .then((result) => response.send(result))
-        .catch((error) => response.send(error));
+    collec.deleteOne({ 
+        task: request.body.task,
+        category: request.body.category
+    })
+        .then((result) => {
+            client.close();
+            response.send(result);
+        })
+        .catch((error) => {
+            client.close();
+            response.send(error);
+        });
+});
+
+app.get("/tasks/:category", (request, response) => {
+    let client = new MongoClient(url);
+    client.connect();
+    let db = client.db("Dailify");
+    let collec = db.collection("Tasks");
+    
+    collec.find({ category: request.params.category }).toArray()
+        .then((result) => {
+            client.close();
+            response.send(result);
+        })
+        .catch((error) => {
+            client.close();
+            response.send(error);
+        });
+});
+
+app.get("/tasks", (request, response) => {
+    let client = new MongoClient(url);
+    client.connect();
+    let db = client.db("Dailify");
+    let collec = db.collection("Tasks");
+    
+    collec.find({}).toArray()
+        .then((result) => {
+            client.close();
+            response.send(result);
+        })
+        .catch((error) => {
+            client.close();
+            response.send(error);
+        });
+});
+
+app.post("/category/add", (request, response) => {
+    let client = new MongoClient(url);
+    client.connect();
+    let db = client.db("Dailify");
+    let collec = db.collection("Categories");
+    
+    let obj = {
+        id: request.body.id,
+        name: request.body.name
+    };
+    
+    collec.insertOne(obj)
+        .then((result) => {
+            client.close();
+            response.send(result);
+        })
+        .catch((error) => {
+            client.close();
+            response.send(error);
+        });
+});
+
+app.get("/categories", (request, response) => {
+    let client = new MongoClient(url);
+    client.connect();
+    let db = client.db("Dailify");
+    let collec = db.collection("Categories");
+    
+    collec.find({}).toArray()
+        .then((result) => {
+            client.close();
+            response.send(result);
+        })
+        .catch((error) => {
+            client.close();
+            response.send(error);
+        });
+});
+
+app.delete("/category/delete", (request, response) => {
+    let client = new MongoClient(url);
+    client.connect();
+    let db = client.db("Dailify");
+    let collec = db.collection("Categories");
+    
+    collec.deleteOne({ id: request.body.id })
+        .then((result) => {
+            client.close();
+            response.send(result);
+        })
+        .catch((error) => {
+            client.close();
+            response.send(error);
+        });
 });
 
 app.listen(9000, () => {
