fb68ad79c26d238de5412edad8c9e0051a151c62
YashRaut24 <yashcodezzzz24@gmail.com>
2025-10-22 19:22:47 +0530

    Added this -> hide home page when profile is opened

--- frontend/my-app/src/components/HomePage.jsx
+++ frontend/my-app/src/components/HomePage.jsx
@@ -3,9 +3,11 @@ import "./HomePage.css";
 import TaskContainer from "./TaskContainer";
 
 function HomePage(props) {
+  if (props.profileVisible) return null; // Hide tasks when profile is visible
+  
   return (
     <div className={`task-container ${props.collapsed ? "expanded" : ""}`}>
-      <TaskContainer mode={props.mode} category={props.selectedTask} />
+      <TaskContainer mode={props.mode} category={props.selectedTask} collapsed={props.collapsed} />
     </div>
   );
 }
