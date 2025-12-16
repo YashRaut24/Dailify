5383b83d47413ba768801d4a8179f98a42c236de
YashRaut24 <yashcodezzzz24@gmail.com>
2025-12-17 00:15:51 +0530

    Set up Express server with database connection and auth routes

--- backend/index.js
+++ backend/index.js
@@ -1,146 +1,15 @@
-let express = require("express");
-let app = express();
-let cors = require("cors");
-let mongodb = require("mongodb");
-let MongoClient = mongodb.MongoClient;
+const express = require("express");
+const connectDB = require("./config/db.js")
+const cors = require("cors");
 
-app.use(cors());
+const app = express();
+
+app.use(cors())
 app.use(express.json());
+connectDB();
 
-const url = "mongodb://localhost:27017";
+app.use("/",require("./routes/authRoutes.js"));
 
-app.post("/add", (request, response) => {
-    let client = new MongoClient(url);
-    client.connect();
-    let db = client.db("Dailify");
-    let collec = db.collection("Tasks");
-    
-    let obj = {
-        task: request.body.task,
-        category: request.body.category
-    };
-    
-    collec.insertOne(obj)
-        .then((result) => {
-            client.close();
-            response.send(result);
-        })
-        .catch((error) => {
-            client.close();
-            response.send(error);
-        });
-});
-
-app.delete("/delete", (request, response) => {
-    let client = new MongoClient(url);
-    client.connect();
-    let db = client.db("Dailify");
-    let collec = db.collection("Tasks");
-    
-    collec.deleteOne({ 
-        task: request.body.task,
-        category: request.body.category
-    })
-        .then((result) => {
-            client.close();
-            response.send(result);
-        })
-        .catch((error) => {
-            client.close();
-            response.send(error);
-        });
-});
-
-app.get("/tasks/:category", (request, response) => {
-    let client = new MongoClient(url);
-    client.connect();
-    let db = client.db("Dailify");
-    let collec = db.collection("Tasks");
-    
-    collec.find({ category: request.params.category }).toArray()
-        .then((result) => {
-            client.close();
-            response.send(result);
-        })
-        .catch((error) => {
-            client.close();
-            response.send(error);
-        });
-});
-
-app.get("/tasks", (request, response) => {
-    let client = new MongoClient(url);
-    client.connect();
-    let db = client.db("Dailify");
-    let collec = db.collection("Tasks");
-    
-    collec.find({}).toArray()
-        .then((result) => {
-            client.close();
-            response.send(result);
-        })
-        .catch((error) => {
-            client.close();
-            response.send(error);
-        });
-});
-
-app.post("/category/add", (request, response) => {
-    let client = new MongoClient(url);
-    client.connect();
-    let db = client.db("Dailify");
-    let collec = db.collection("Categories");
-    
-    let obj = {
-        id: request.body.id,
-        name: request.body.name
-    };
-    
-    collec.insertOne(obj)
-        .then((result) => {
-            client.close();
-            response.send(result);
-        })
-        .catch((error) => {
-            client.close();
-            response.send(error);
-        });
-});
-
-app.get("/categories", (request, response) => {
-    let client = new MongoClient(url);
-    client.connect();
-    let db = client.db("Dailify");
-    let collec = db.collection("Categories");
-    
-    collec.find({}).toArray()
-        .then((result) => {
-            client.close();
-            response.send(result);
-        })
-        .catch((error) => {
-            client.close();
-            response.send(error);
-        });
-});
-
-app.delete("/category/delete", (request, response) => {
-    let client = new MongoClient(url);
-    client.connect();
-    let db = client.db("Dailify");
-    let collec = db.collection("Categories");
-    
-    collec.deleteOne({ id: request.body.id })
-        .then((result) => {
-            client.close();
-            response.send(result);
-        })
-        .catch((error) => {
-            client.close();
-            response.send(error);
-        });
-});
-
-app.listen(9000, () => {
-    console.log("Express is Active on port 9000");
+app.listen(9000, ()=>{
+    console.log("Server running on port 9000");
 });
