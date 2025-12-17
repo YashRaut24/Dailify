2fd5032ac8da44041e4617d3d9f8a7776c34c36f
YashRaut24 <yashcodezzzz24@gmail.com>
2025-12-17 20:31:43 +0530

    Implement user authentication with signup field

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -2,20 +2,63 @@ const express = require("express");
 const router = express.Router()
 const User = require("../models/User");
 
-router.post("/signup",async (req,res)=>{
-    const { name, email, about, location, password} = req.body;
+router.post("/signup", async (req, res) => {
+  const { name, email, bio, location, password } = req.body;
 
-    if(!name || !email || !about || !location || password){
-        return res.status(400).json({message: "All fields required"});
-    }
+  if (!name || !email || !password) {
+    return res.status(400).json({ message: "Name, email and password are required" });
+  }
 
-    try{
-        const user = new User({name, email, about, location, password});
-        await user.save();
-        res.status(201).json({ message: "User registered" });
+  try {
+    const user = new User({
+      name,
+      email,
+      bio,
+      location,
+      password
+    });
+
+    await user.save();
+    res.status(201).json({ message: "User registered successfully" });
+
   } catch (err) {
+    console.error(err);
     res.status(400).json({ message: "Email already exists" });
   }
 });
+
+
+router.post("/signin", async (req,res) => {
+  try{
+    const {email, password} = req.body;
+
+    if(!email || !password){
+      return res.status(400).json({message: "All fields required"});
+    }
+
+    const user = await User.findOne({email});
+
+    if(!user){
+      return res.status(401).json({message: "Invalid credentials"});
+    }
+
+    const isMatch = await bcrypt.compare(password,user.password);
+
+    if(!isMatch){
+      return res.status(401).json({message: "Invalid credentials"});
+    }
+    res.status(200).json({
+        message: "Signin successful",
+        user: {
+          id: user._id,
+          name: user.name,
+          email: user.email
+        }
+      });
+
+    } catch (err) {
+    res.status(500).json({ message: "Server error" });
+  }
+})
 
 module.exports = router;
