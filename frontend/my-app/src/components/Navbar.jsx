0e786f21646e5041684a3aff3d4dea6668e807b1
YashRaut24 <yashcodezzzz24@gmail.com>
2025-12-19 16:55:03 +0530

    Added logout button

--- frontend/my-app/src/components/Navbar.jsx
+++ frontend/my-app/src/components/Navbar.jsx
@@ -3,22 +3,37 @@ import './Navbar.css';
 import { MdDarkMode } from "react-icons/md";
 import { MdOutlineLightMode } from "react-icons/md";
 import { CgProfile } from "react-icons/cg";
+import { GrLogout } from "react-icons/gr";
+import { Link } from "react-router-dom";
 
-function Navbar(props){
+function Navbar(props) {
+  const toggleProfile = (e) => {
+    e.preventDefault();
+    props.setProfileVisible(!props.profileVisible);
+  };
 
-    const toggleProfile = (e) => {
-        e.preventDefault();
-        props.setProfileVisible(!props.profileVisible);
-    } 
+  return (
+    <div className={props.mode ? "darkNavbar" : "navbar"}>
+      <button className="theme-toggle" onClick={props.changeTheme}>
+        {props.mode ? <MdOutlineLightMode /> : <MdDarkMode />}
+      </button>
 
-    return <div className={props.mode? "darkNavbar":"navbar"}>
-            <button className="theme-toggle" onClick={props.changeTheme}>
-                {props.mode ? <MdOutlineLightMode/> : <MdDarkMode/>}
-            </button>
-            <button onClick={toggleProfile} className={props.mode? "dark-profile" : "profile"}>
-                <CgProfile />
-            </button>
-        </div>
+      <div className="nav-bottom">
+        <button
+          onClick={toggleProfile}
+          className={props.mode ? "dark-profile" : "profile"}
+        >
+          <CgProfile />
+        </button>
+
+        <button className="logout-button">
+          <Link to="/">
+            <GrLogout />
+          </Link>
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
